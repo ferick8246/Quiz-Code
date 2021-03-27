9832ae2f078daadf52454700cd41e8439271922f
Erick Flore <ferick8246@gmail.com>
2021-03-26 20:22:40 -0500

    added funtion to save highscore

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -117,4 +117,39 @@ function quizEnd() {
     // hide questions section
     questionsEl.setAttribute("class", "hide");
   }
+ 
+  function clockTick() {
+    // update time
+    time--;
+    timerEl.textContent = time;
   
+    // check if user ran out of time
+    if (time <= 0) {
+      quizEnd();
+    }
+  }
+  
+  function saveHighscore() {
+    // get value of input box
+    var initials = initialsEl.value.trim();
+  
+    // make sure value wasn't empty
+    if (initials !== "") {
+      // get saved scores from localstorage, or if not any, set to empty array
+      var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+      // format new score object for current user
+      var newScore = {
+        score: time,
+        initials: initials
+      };
+  
+      // save to localstorage
+      highscores.push(newScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+  
+      // redirect to next page
+      window.location.href = "highscores.html";
+    }
+  }
